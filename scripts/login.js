05d214a56f96c69803b570975f02b9e93b4dc5c2
Ynse Vandenbilcke <ynse.vandenbilcke@icloud.com>
2019-02-26 13:43:31 +0100

    Working on selection screen

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,18 @@
 function setup() {
 }
 
+let usrInfo = {
+    "usernames": {
+        "username1":"UniverZ",
+        "username2":"Petrovic258",
+        "username3":"TechNyx"
+    },
+    "passwords": {
+        "password1":"test1",
+        "password2":"test2",
+        "password3":"test3"
+    }
+};
+
+
 window.addEventListener("load", setup);
